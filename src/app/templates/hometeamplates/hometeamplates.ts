536fd40bc6ee7f2ca65edc6b8e46f29bba7e2124
epshita-shima <epshitashima@.com>
2025-07-03 14:24:48 +0600

    event listener

--- src/app/templates/hometeamplates/hometeamplates.ts
+++ src/app/templates/hometeamplates/hometeamplates.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-hometeamplates',
-  imports: [],
+  imports: [CommonModule,FormsModule ],
   templateUrl: './hometeamplates.html',
   styleUrl: './hometeamplates.css'
 })
 export class Hometeamplates {
+  @ViewChild('usernameInput') usernameInput!: ElementRef;
   // Text Interpolation
   userName = 'Shima Khatun';
   // Property Binding
@@ -14,7 +16,40 @@ export class Hometeamplates {
   //  Attribute Binding
   isDisabled = true;
   // Class Binding
-    isActive = true;
-//  Style Binding 
+  isActive = true;
+  //  Style Binding 
   fontColor = 'green';
+
+  handleClick() {
+    alert('Button Clicked!');
+  }
+
+  message = '';
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.message = input.value;
+  }
+
+  showEventDetails(event: MouseEvent) {
+    console.log('Event Type:', event.type);
+    console.log('X Position:', event.clientX);
+  }
+
+  submitForm(event: Event) {
+    event.preventDefault(); // default reload বন্ধ
+    const username=this.usernameInput.nativeElement.value;
+    console.log('Form submitted!',username);
+  }
+
+  onEnterKey(){
+    alert('you pressed enter')
+  }
+
+  hovering = false;
+
+  name = '';
+
+onNameChange() {
+  console.log('Name changed to:', this.name);
+}
 }
